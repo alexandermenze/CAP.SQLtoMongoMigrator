9d45d97e42d52de124ea59cd001b206d4cda10f9
Alexander Menze <a.menze@polipol.de>
2018-04-09 07:58:33 +0200

    Initialized project, db connections

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+
+namespace CAP.SQLToMongoMigrator
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var mongoClient = new MongoClient();
+        }
+    }
+}
