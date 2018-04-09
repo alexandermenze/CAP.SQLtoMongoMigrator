f5471a70a1401d50af04688ed479eacd4b852a8d
Alexander Menze <a.menze@polipol.de>
2018-04-09 09:08:48 +0200

    Created thread pool state model

---
+++ Model/MigratorState.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace CAP.SQLToMongoMigrator.Model
+{
+    internal class MigratorState
+    {
+        public SqlServerLogsEntities SqlClient { get; set; }
+        public MongoClient MongoClient { get; set; }
+        public IntRange IdRange { get; set; }
+    }
+}
